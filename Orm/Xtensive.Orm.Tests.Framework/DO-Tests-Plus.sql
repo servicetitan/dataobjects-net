9f26f2cf127b2c8131cf4c9cb438b7107a7650bc
Sergei Pavlov <spavlov@servicetitan.com>
2023-04-06 14:09:52 -0700

    Remove FTS scripts

--- Orm/Xtensive.Orm.Tests.Framework/DO-Tests-Plus.sql
+++ Orm/Xtensive.Orm.Tests.Framework/DO-Tests-Plus.sql
@@ -28,21 +28,6 @@ EXEC [dbo].[sp_fulltext_database] @action = 'enable'
 end
 GO
 
--- And creating full-text catalogue
-DECLARE @server_directory NVARCHAR(520)
-SELECT @server_directory = SUBSTRING(filename, 1, CHARINDEX(N'DATA\master.mdf', LOWER(filename)) - 1)
-FROM master.dbo.sysaltfiles WHERE dbid = 1 AND fileid = 1
-
-DECLARE @ftdata_directory NVARCHAR(520)
-SELECT @ftdata_directory = @server_directory + N'FTData\'
-
-EXECUTE (N'CREATE FULLTEXT CATALOG [Default]
-IN PATH N''' + @ftdata_directory + N'DO-Tests-1''
-WITH ACCENT_SENSITIVITY = ON
-AS DEFAULT
-AUTHORIZATION [dbo]')
-GO
-
 --------------
 -- DO-Tests-2
 --------------
@@ -54,17 +39,7 @@ if exists (select * from sysdatabases where name='DO-Tests-2')
   drop database [DO-Tests-2]
 GO
 
--- Creating the database
-DECLARE @server_directory NVARCHAR(520)
-SELECT @server_directory = SUBSTRING(filename, 1, CHARINDEX(N'DATA\master.mdf', LOWER(filename)) - 1)
-FROM master.dbo.sysaltfiles WHERE dbid = 1 AND fileid = 1
-
-DECLARE @data_directory NVARCHAR(520)
-SELECT @data_directory = @server_directory + N'DATA\'
-
-EXECUTE (N'CREATE DATABASE [DO-Tests-2]
-  ON PRIMARY (NAME = N''DO-Tests-2'', FILENAME = N''' + @data_directory + N'DO-Tests-2.mdf'')
-  LOG ON (NAME = N''DO-Tests-2_log'',  FILENAME = N''' + @data_directory + N'DO-Tests-2.ldf'')')
+CREATE DATABASE [DO-Tests-2]
 GO
 
 USE [DO-Tests-2]
@@ -83,21 +58,6 @@ EXEC [dbo].[sp_fulltext_database] @action = 'enable'
 end
 GO
 
--- And creating full-text catalogue
-DECLARE @server_directory NVARCHAR(520)
-SELECT @server_directory = SUBSTRING(filename, 1, CHARINDEX(N'DATA\master.mdf', LOWER(filename)) - 1)
-FROM master.dbo.sysaltfiles WHERE dbid = 1 AND fileid = 1
-
-DECLARE @ftdata_directory NVARCHAR(520)
-SELECT @ftdata_directory = @server_directory + N'FTData\'
-
-EXECUTE (N'CREATE FULLTEXT CATALOG [Default]
-IN PATH N''' + @ftdata_directory + N'DO-Tests-2''
-WITH ACCENT_SENSITIVITY = ON
-AS DEFAULT
-AUTHORIZATION [dbo]')
-GO
-
 --------------
 -- DO-Tests-3
 --------------
@@ -109,17 +69,8 @@ if exists (select * from sysdatabases where name='DO-Tests-3')
   drop database [DO-Tests-3]
 GO
 
--- Creating the database
-DECLARE @server_directory NVARCHAR(520)
-SELECT @server_directory = SUBSTRING(filename, 1, CHARINDEX(N'DATA\master.mdf', LOWER(filename)) - 1)
-FROM master.dbo.sysaltfiles WHERE dbid = 1 AND fileid = 1
 
-DECLARE @data_directory NVARCHAR(520)
-SELECT @data_directory = @server_directory + N'DATA\'
-
-EXECUTE (N'CREATE DATABASE [DO-Tests-3]
-  ON PRIMARY (NAME = N''DO-Tests-3'', FILENAME = N''' + @data_directory + N'DO-Tests-3.mdf'')
-  LOG ON (NAME = N''DO-Tests-3_log'',  FILENAME = N''' + @data_directory + N'DO-Tests-3.ldf'')')
+CREATE DATABASE [DO-Tests-3]
 GO
 
 USE [DO-Tests-3]
@@ -138,20 +89,6 @@ EXEC [dbo].[sp_fulltext_database] @action = 'enable'
 end
 GO
 
--- And creating full-text catalogue
-DECLARE @server_directory NVARCHAR(520)
-SELECT @server_directory = SUBSTRING(filename, 1, CHARINDEX(N'DATA\master.mdf', LOWER(filename)) - 1)
-FROM master.dbo.sysaltfiles WHERE dbid = 1 AND fileid = 1
-
-DECLARE @ftdata_directory NVARCHAR(520)
-SELECT @ftdata_directory = @server_directory + N'FTData\'
-
-EXECUTE (N'CREATE FULLTEXT CATALOG [Default]
-IN PATH N''' + @ftdata_directory + N'DO-Tests-3''
-WITH ACCENT_SENSITIVITY = ON
-AS DEFAULT
-AUTHORIZATION [dbo]')
-GO
 
 --------------
 -- DO-Tests-4
@@ -164,17 +101,7 @@ if exists (select * from sysdatabases where name='DO-Tests-4')
   drop database [DO-Tests-4]
 GO
 
--- Creating the database
-DECLARE @server_directory NVARCHAR(520)
-SELECT @server_directory = SUBSTRING(filename, 1, CHARINDEX(N'DATA\master.mdf', LOWER(filename)) - 1)
-FROM master.dbo.sysaltfiles WHERE dbid = 1 AND fileid = 1
-
-DECLARE @data_directory NVARCHAR(520)
-SELECT @data_directory = @server_directory + N'DATA\'
-
-EXECUTE (N'CREATE DATABASE [DO-Tests-4]
-  ON PRIMARY (NAME = N''DO-Tests-4'', FILENAME = N''' + @data_directory + N'DO-Tests-4.mdf'')
-  LOG ON (NAME = N''DO-Tests-4_log'',  FILENAME = N''' + @data_directory + N'DO-Tests-4.ldf'')')
+CREATE DATABASE [DO-Tests-4
 GO
 
 USE [DO-Tests-4]
@@ -193,17 +120,3 @@ EXEC [dbo].[sp_fulltext_database] @action = 'enable'
 end
 GO
 
--- And creating full-text catalogue
-DECLARE @server_directory NVARCHAR(520)
-SELECT @server_directory = SUBSTRING(filename, 1, CHARINDEX(N'DATA\master.mdf', LOWER(filename)) - 1)
-FROM master.dbo.sysaltfiles WHERE dbid = 1 AND fileid = 1
-
-DECLARE @ftdata_directory NVARCHAR(520)
-SELECT @ftdata_directory = @server_directory + N'FTData\'
-
-EXECUTE (N'CREATE FULLTEXT CATALOG [Default]
-IN PATH N''' + @ftdata_directory + N'DO-Tests-4''
-WITH ACCENT_SENSITIVITY = ON
-AS DEFAULT
-AUTHORIZATION [dbo]')
-GO
