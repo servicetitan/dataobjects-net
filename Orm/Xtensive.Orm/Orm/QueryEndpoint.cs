d68f27816b9643f7e0bdd93654e7fc509ca65015
Sergei Pavlov <spavlov@servicetitan.com>
2023-05-16 11:26:14 -0700

    Add .ConfigureAwait(false)

--- Orm/Xtensive.Orm/Orm/QueryEndpoint.cs
+++ Orm/Xtensive.Orm/Orm/QueryEndpoint.cs
@@ -289,7 +289,7 @@ namespace Xtensive.Orm
       if (key is null) {
         return null;
       }
-      var result = await SingleOrDefaultAsync(key, ct);
+      var result = await SingleOrDefaultAsync(key, ct).ConfigureAwait(false);
       if (result is null) {
         ThrowKeyNotFoundException(key);
       }
@@ -357,14 +357,14 @@ namespace Xtensive.Orm
           OrmLog.Debug(nameof(Strings.LogSessionXResolvingKeyYExactTypeIsZ), session, key, key.HasExactType ? Strings.Known : Strings.Unknown);
         }
 
-        state = await session.Handler.FetchEntityStateAsync(key, ct);
+        state = await session.Handler.FetchEntityStateAsync(key, ct).ConfigureAwait(false);
       }
       else if (state.Tuple == null) {
         var stateKeyType = state.Key.TypeReference.Type.UnderlyingType;
         var keyType = key.TypeReference.Type.UnderlyingType;
         if (stateKeyType != keyType && !stateKeyType.IsAssignableFrom(keyType)) {
           session.RemoveStateFromCache(state.Key, true);
-          state = await session.Handler.FetchEntityStateAsync(key, ct);
+          state = await session.Handler.FetchEntityStateAsync(key, ct).ConfigureAwait(false);
         }
       }
       var result = state == null || state.IsNotAvailableOrMarkedAsRemoved
