972d1fdb2ee068cb8964a5af7950902074e7aaea
Sergei Pavlov <spavlov@servicetitan.com>
2023-10-02 21:36:38 -0700

    Fix Array.Resize() usages

--- Orm/Xtensive.Orm/Orm/Model/TypeIdRegistry.cs
+++ Orm/Xtensive.Orm/Orm/Model/TypeIdRegistry.cs
@@ -123,9 +123,9 @@ namespace Xtensive.Orm.Model
         if (typeId <= UInt16.MaxValue && type.SharedId <= UInt16.MaxValue) {
           sharedIdToTypeId ??= new UInt16[1];
           typeIdToSharedId ??= new UInt16[1];
-          Array.Resize(ref sharedIdToTypeId, Math.Max(type.SharedId + 1, sharedIdToTypeInfo.Count));
+          Array.Resize(ref sharedIdToTypeId, Math.Max(type.SharedId + 1, Math.Max(sharedIdToTypeInfo.Count, sharedIdToTypeId.Length)));
           sharedIdToTypeId[type.SharedId] = (UInt16)typeId;
-          Array.Resize(ref typeIdToSharedId, typeId + 1);
+          Array.Resize(ref typeIdToSharedId, Math.Max(typeIdToSharedId.Length, typeId + 1));
           typeIdToSharedId[typeId] = (UInt16) type.SharedId;
           return;
         }
