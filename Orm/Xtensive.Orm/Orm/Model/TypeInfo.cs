6f7044be2bd601d9f28d11b571e54c5eeac5b63b
Sergei Pavlov <spavlov@servicetitan.com>
2023-11-14 02:20:35 -0800

    rename

--- Orm/Xtensive.Orm/Orm/Model/TypeInfo.cs
+++ Orm/Xtensive.Orm/Orm/Model/TypeInfo.cs
@@ -923,11 +923,11 @@ namespace Xtensive.Orm.Model
       if (type == typeof(Entity)) {
         return new[] { Fields[nameof(Entity.TypeId)] };
       }
-      var tokens = type.GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).Select(p => (p.MetadataToken, p.Name)).ToHashSet();
+      var declared = type.GetProperties(BindingFlags.DeclaredOnly | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance).Select(p => (p.MetadataToken, p.Name)).ToHashSet();
       return GetBaseFields(type.BaseType, fields)
         .Concat(
           // MetadataToken key would not be enough because of possible collisions between modules
-          fields.Where(p => tokens.Contains((p.UnderlyingProperty.MetadataToken, p.UnderlyingProperty.Name)))
+          fields.Where(p => declared.Contains((p.UnderlyingProperty.MetadataToken, p.UnderlyingProperty.Name)))
             .OrderBy(p => p.UnderlyingProperty.MetadataToken)
         );
     }
