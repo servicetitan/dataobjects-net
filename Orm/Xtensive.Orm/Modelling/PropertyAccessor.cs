f304902c198548dccad950a0cdc13305b1ee3f9d
Sergei Pavlov <spavlov@servicetitan.com>
2023-11-21 15:16:41 -0800

    Remove unused line

--- Orm/Xtensive.Orm/Modelling/PropertyAccessor.cs
+++ Orm/Xtensive.Orm/Modelling/PropertyAccessor.cs
@@ -174,7 +174,6 @@ namespace Xtensive.Modelling
         dependencyRootType = pa.DependencyRootType;
         compareCaseInsensitive = tProperty == WellKnownTypes.String && pa.CaseInsensitiveComparison;
       }
-      var invoker = InnerInitializeMethodDefinition.CachedMakeGenericMethod(tType, tProperty);
 #if NET8_0_OR_GREATER
       InnerInitializeMethodDefinition.CachedMakeGenericMethodInvoker(tType, tProperty).Invoke(this);
 #else
