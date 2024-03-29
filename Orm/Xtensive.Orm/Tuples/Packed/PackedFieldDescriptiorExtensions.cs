32c8f379316fdbe92ce9f7fb436c45591d6a67f4
Sergei Pavlov <spavlov@servicetitan.com>
2024-03-28 23:58:07 -0700

    Remove `in` specifier for not-readonly struct `PackedFieldDescriptor`

--- Orm/Xtensive.Orm/Tuples/Packed/PackedFieldDescriptiorExtensions.cs
+++ Orm/Xtensive.Orm/Tuples/Packed/PackedFieldDescriptiorExtensions.cs
@@ -9,12 +9,12 @@ namespace Xtensive.Tuples.Packed
     private const int OffsetBitCount = 6;
     private const int OffsetMask = (1 << OffsetBitCount) - 1;
 
-    public static PackedFieldAccessor GetAccessor(in this PackedFieldDescriptor descriptor) => PackedFieldAccessor.All[descriptor.AccessorIndex];
-    public static bool IsObjectField(in this PackedFieldDescriptor descriptor) => descriptor.GetAccessor().Rank < 0;
-    public static int GetObjectIndex(in this PackedFieldDescriptor descriptor) => descriptor.DataPosition;
-    public static int GetValueIndex(in this PackedFieldDescriptor descriptor) => descriptor.DataPosition >> OffsetBitCount;
-    public static int GetValueBitOffset(in this PackedFieldDescriptor descriptor) => descriptor.DataPosition & OffsetMask;
-    public static int GetStateIndex(in this PackedFieldDescriptor descriptor) => descriptor.StatePosition >> OffsetBitCount;
-    public static int GetStateBitOffset(in this PackedFieldDescriptor descriptor) => descriptor.StatePosition & OffsetMask;
+    public static PackedFieldAccessor GetAccessor(this PackedFieldDescriptor descriptor) => PackedFieldAccessor.All[descriptor.AccessorIndex];
+    public static bool IsObjectField(this PackedFieldDescriptor descriptor) => descriptor.GetAccessor().Rank < 0;
+    public static int GetObjectIndex(this PackedFieldDescriptor descriptor) => descriptor.DataPosition;
+    public static int GetValueIndex(this PackedFieldDescriptor descriptor) => descriptor.DataPosition >> OffsetBitCount;
+    public static int GetValueBitOffset(this PackedFieldDescriptor descriptor) => descriptor.DataPosition & OffsetMask;
+    public static int GetStateIndex(this PackedFieldDescriptor descriptor) => descriptor.StatePosition >> OffsetBitCount;
+    public static int GetStateBitOffset(this PackedFieldDescriptor descriptor) => descriptor.StatePosition & OffsetMask;
   }
 }
