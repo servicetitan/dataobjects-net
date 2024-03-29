960122083dfd171560c4bec0f6b5fe4e6d64bd29
Sergei Pavlov <spavlov@servicetitan.com>
2024-03-29 00:04:12 -0700

    Add `[MethodImpl(MethodImplOptions.AggressiveInlining)]` attribute

--- Orm/Xtensive.Orm/Tuples/Packed/PackedFieldDescriptiorExtensions.cs
+++ Orm/Xtensive.Orm/Tuples/Packed/PackedFieldDescriptiorExtensions.cs
@@ -2,6 +2,8 @@
 // This code is distributed under MIT license terms.
 // See the License.txt file in the project root for more information.
 
+using System.Runtime.CompilerServices;
+
 namespace Xtensive.Tuples.Packed
 {
   internal static class PackedFieldDescriptorExtensions
@@ -9,12 +11,25 @@ namespace Xtensive.Tuples.Packed
     private const int OffsetBitCount = 6;
     private const int OffsetMask = (1 << OffsetBitCount) - 1;
 
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static PackedFieldAccessor GetAccessor(this PackedFieldDescriptor descriptor) => PackedFieldAccessor.All[descriptor.AccessorIndex];
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool IsObjectField(this PackedFieldDescriptor descriptor) => descriptor.GetAccessor().Rank < 0;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static int GetObjectIndex(this PackedFieldDescriptor descriptor) => descriptor.DataPosition;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static int GetValueIndex(this PackedFieldDescriptor descriptor) => descriptor.DataPosition >> OffsetBitCount;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static int GetValueBitOffset(this PackedFieldDescriptor descriptor) => descriptor.DataPosition & OffsetMask;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static int GetStateIndex(this PackedFieldDescriptor descriptor) => descriptor.StatePosition >> OffsetBitCount;
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static int GetStateBitOffset(this PackedFieldDescriptor descriptor) => descriptor.StatePosition & OffsetMask;
   }
 }
