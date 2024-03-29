328020e8530fc15ad695bdc6eb54a67a529a929e
Sergei Pavlov <spavlov@servicetitan.com>
2024-03-29 13:25:19 -0700

    `DO_MAX_1000_COLUMNS` conditional compilation

--- Orm/Xtensive.Orm/Tuples/Packed/PackedFieldDescriptor.cs
+++ Orm/Xtensive.Orm/Tuples/Packed/PackedFieldDescriptor.cs
@@ -12,11 +12,61 @@ namespace Xtensive.Tuples.Packed
   [Serializable]
   internal struct PackedFieldDescriptor
   {
+#if DO_MAX_1000_COLUMNS
+    private int bitFields;
+
+    internal int Index
+    {
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      get => bitFields & 0x7FF;
+
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      set => bitFields =  bitFields & ~0x7FF | value;
+    }
+
+    internal int StateIndex
+    {
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      get => (bitFields >> 11) & 0x1F;
+
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      set => bitFields = bitFields & ~(0x1F << 11) | (value << 11);
+    }
+
+    internal int ValueBitOffset
+    {
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      get => (bitFields >> 16) & 0x3F;
+
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      set => bitFields = bitFields & ~0x3F00 | (value << 16);
+    }
+
+    internal int StateBitOffset
+    {
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      get => (bitFields >> 21) & 0x3E;                                // Even always
+
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      set => bitFields = bitFields & ~(0x3E << 21) | (value << 21);
+    }
+
+    internal int AccessorIndex
+    {
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      get => (bitFields >> 27) & 0x1F;
+
+      [MethodImpl(MethodImplOptions.AggressiveInlining)]
+      set => bitFields = bitFields & ~(0x1F << 27) | (value << 27);
+    }
+#else
     internal ushort Index;
     internal ushort StateIndex;
     internal byte ValueBitOffset;
     internal byte StateBitOffset;
     internal byte AccessorIndex;
+
+#endif // DO_MAX_1000_COLUMNS
 
     internal int DataPosition
     {
