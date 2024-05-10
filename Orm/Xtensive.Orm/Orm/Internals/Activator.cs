1f46302971e4f68bf77564ce28e090f51868437a
Sergei Pavlov <spavlov@servicetitan.com>
2024-05-10 14:57:05 -0700

    Optimize Activator

--- Orm/Xtensive.Orm/Orm/Internals/Activator.cs
+++ Orm/Xtensive.Orm/Orm/Internals/Activator.cs
@@ -19,53 +19,40 @@ namespace Xtensive.Orm.Internals
     private const string OrmFactoryMethodName = "CreateObject";
     private const string OtherFactoryMethodName = "~Xtensive.Orm.CreateObject";
 
-    private static readonly ConcurrentDictionary<Type, Func<Session, EntityState, Entity>> EntityActivators
-      = new ConcurrentDictionary<Type, Func<Session, EntityState, Entity>>();
-    private static readonly ConcurrentDictionary<Type, Func<Session, Tuple, Structure>> DetachedStructureActivators
-      = new ConcurrentDictionary<Type, Func<Session, Tuple, Structure>>();
-    private static readonly ConcurrentDictionary<Type, Func<Persistent, FieldInfo, Structure>> StructureActivators
-      = new ConcurrentDictionary<Type, Func<Persistent, FieldInfo, Structure>>();
-    private static readonly ConcurrentDictionary<Type, Func<Entity, FieldInfo, EntitySetBase>> EntitySetActivators
-      = new ConcurrentDictionary<Type, Func<Entity, FieldInfo, EntitySetBase>>();
-
-    public static Entity CreateEntity(Session session, Type type, EntityState state)
+    private static class Traits<TArg1, TArg2, TResult> where TArg1 : class where TArg2 : class
     {
-      var activator = EntityActivators.GetOrAdd(type, GetActivator<Session, EntityState, Entity>);
-      return activator.Invoke(session, state);
+      private static readonly Type DelegateType = typeof(Func<TArg1, TArg2, TResult>);
+      private static readonly Type[] TypeParams = [typeof(TArg1), typeof(TArg2)];
+
+      public static readonly Func<Type, Func<TArg1, TArg2, TResult>> Activator = type => {
+        var methodName = type.Assembly == OrmAssembly ? OrmFactoryMethodName : OtherFactoryMethodName;
+        var method = type.GetMethod(methodName, BindingFlags.Static | BindingFlags.NonPublic, null, TypeParams, null)
+                     ?? throw new InvalidOperationException(string.Format(
+                       Strings.ExUnableToFindFactoryMethodForTypeXMakeSureAssemblyYProcessedByWeaver,
+                       type.FullName, type.Assembly));
+        return (Func<TArg1, TArg2, TResult>) Delegate.CreateDelegate(DelegateType, method);
+      };
     }
+
+    private static readonly ConcurrentDictionary<Type, Func<Session, EntityState, Entity>> EntityActivators = new();
+    private static readonly ConcurrentDictionary<Type, Func<Session, Tuple, Structure>> DetachedStructureActivators = new();
+    private static readonly ConcurrentDictionary<Type, Func<Persistent, FieldInfo, Structure>> StructureActivators = new();
+    private static readonly ConcurrentDictionary<Type, Func<Entity, FieldInfo, EntitySetBase>> EntitySetActivators = new();
+
+    public static Entity CreateEntity(Session session, Type type, EntityState state) =>
+      EntityActivators.GetOrAdd(type, Traits<Session, EntityState, Entity>.Activator).Invoke(session, state);
 
     public static Structure CreateStructure(Type type, Persistent owner, FieldInfo field)
     {
-      var activator = StructureActivators.GetOrAdd(type, GetActivator<Persistent, FieldInfo, Structure>);
-      var result = activator.Invoke(owner, field);
+      var result = StructureActivators.GetOrAdd(type, Traits<Persistent, FieldInfo, Structure>.Activator).Invoke(owner, field);
       result.SystemInitialize(true);
       return result;
     }
 
-    public static Structure CreateStructure(Session session, Type type, Tuple tuple)
-    {
-      var activator = DetachedStructureActivators.GetOrAdd(type, GetActivator<Session, Tuple, Structure>);
-      return activator.Invoke(session, tuple);
-    }
+    public static Structure CreateStructure(Session session, Type type, Tuple tuple) =>
+      DetachedStructureActivators.GetOrAdd(type, Traits<Session, Tuple, Structure>.Activator).Invoke(session, tuple);
 
-    public static EntitySetBase CreateEntitySet(Entity owner, FieldInfo field)
-    {
-      var activator = EntitySetActivators.GetOrAdd(field.ValueType, GetActivator<Entity, FieldInfo, EntitySetBase>);
-      return activator.Invoke(owner, field);
-    }
-
-    private static Func<TArg1, TArg2, TResult> GetActivator<TArg1, TArg2, TResult>(Type type)
-      where TArg1 : class
-      where TArg2 : class
-    {
-      var methodName = type.Assembly==OrmAssembly ? OrmFactoryMethodName : OtherFactoryMethodName;
-      var parameters = new[] {typeof (TArg1), typeof (TArg2)};
-      var method = type.GetMethod(methodName, BindingFlags.Static | BindingFlags.NonPublic, null, parameters, null);
-      if (method==null)
-        throw new InvalidOperationException(string.Format(
-          Strings.ExUnableToFindFactoryMethodForTypeXMakeSureAssemblyYProcessedByWeaver,
-          type.FullName, type.Assembly));
-      return (Func<TArg1, TArg2, TResult>) Delegate.CreateDelegate(typeof (Func<TArg1, TArg2, TResult>), method);
-    }
+    public static EntitySetBase CreateEntitySet(Entity owner, FieldInfo field) =>
+      EntitySetActivators.GetOrAdd(field.ValueType, Traits<Entity, FieldInfo, EntitySetBase>.Activator).Invoke(owner, field);
   }
 }
