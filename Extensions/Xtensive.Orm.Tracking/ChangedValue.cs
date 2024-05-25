e3a682db8303215018280ee69728b19055d118a7
Sergei Pavlov <spavlov@servicetitan.com>
2024-05-24 20:58:34 -0700

    Convert `ChangedValue` to record syntax

--- Extensions/Xtensive.Orm.Tracking/ChangedValue.cs
+++ Extensions/Xtensive.Orm.Tracking/ChangedValue.cs
@@ -4,39 +4,9 @@
 
 using Xtensive.Orm.Model;
 
-namespace Xtensive.Orm.Tracking
-{
-  /// <summary>
-  /// Represents a pair of original and changed values for a persistent field
-  /// </summary>
-  public readonly struct ChangedValue
-  {
-    /// <summary>
-    /// Gets the field.
-    /// </summary>
-    public FieldInfo Field { get; }
+namespace Xtensive.Orm.Tracking;
 
-    /// <summary>
-    /// Gets the original value.
-    /// </summary>
-    public object OriginalValue { get; }
-
-    /// <summary>
-    /// Gets the new value.
-    /// </summary>
-    public object NewValue { get; }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="ChangedValue"/> class.
-    /// </summary>
-    /// <param name="field">The field.</param>
-    /// <param name="originalValue">The original value.</param>
-    /// <param name="newValue">The new value.</param>
-    public ChangedValue(FieldInfo field, object originalValue, object newValue)
-    {
-      Field = field;
-      OriginalValue = originalValue;
-      NewValue = newValue;
-    }
-  }
-}
+/// <summary>
+/// Represents a pair of original and changed values for a persistent field
+/// </summary>
+public readonly record struct ChangedValue(FieldInfo Field, object OriginalValue, object NewValue);
