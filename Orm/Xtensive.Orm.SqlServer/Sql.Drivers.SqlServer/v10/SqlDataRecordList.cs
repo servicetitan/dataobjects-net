9ac8b013340171141dc08f7f4612d1ea36abb898
Sergei Pavlov <spavlov@servicetitan.com>
2024-09-14 02:21:33 -0700

    Refactor

--- Orm/Xtensive.Orm.SqlServer/Sql.Drivers.SqlServer/v10/SqlDataRecordList.cs
+++ Orm/Xtensive.Orm.SqlServer/Sql.Drivers.SqlServer/v10/SqlDataRecordList.cs
@@ -5,40 +5,39 @@ using System.Linq;
 using Microsoft.Data.SqlClient.Server;
 using Tuple = Xtensive.Tuples.Tuple;
 
-namespace Xtensive.Sql.Drivers.SqlServer.v10
-{
-  public class SqlDataRecordList : List<SqlDataRecord>
-  {
-    public override string ToString() =>
-      $"[{string.Join(", ", this.Select(o => o.GetValue(0) switch {
-        string s => $"\"{s}\"",
-        var ns => ns.ToString()
-      }))}]";
+namespace Xtensive.Sql.Drivers.SqlServer.v10;
 
-    public SqlDataRecordList(IReadOnlyList<Tuple> tuples, SqlDbType sqlDbType)
-      : base(tuples.Count)
-    {
-      SqlMetaData[] metaDatas = null;
-      foreach (var valueObj in tuples.Select(t => t.GetValueOrDefault(0)).Where(o => o != null)) {
-        metaDatas ??= [
-          sqlDbType == SqlDbType.BigInt
-            ? new SqlMetaData("Value", sqlDbType)
-            : new SqlMetaData("Value", sqlDbType, tuples.Max(t => (t.GetValueOrDefault(0) as string)?.Length ?? 20))
-        ];
-        var castValue = valueObj switch {
-          byte n => (long) n,
-          short n => (long) n,
-          ushort n => (long) n,
-          int n => (long) n,
-          uint n => (long) n,
-          decimal d => (long) d,
-          Enum e => Convert.ToInt64(e),
-          var o => o
-        };
-        SqlDataRecord record = new(metaDatas);
-        record.SetValue(0, castValue);
-        Add(record);
-      }
+public class SqlDataRecordList : List<SqlDataRecord>
+{
+  public override string ToString() =>
+    $"[{string.Join(", ", this.Select(o => o.GetValue(0) switch {
+      string s => $"\"{s}\"",
+      var ns => ns.ToString()
+    }))}]";
+
+  public SqlDataRecordList(IReadOnlyList<Tuple> tuples, SqlDbType sqlDbType)
+    : base(tuples.Count)
+  {
+    SqlMetaData[] metaDatas = null;
+    foreach (var valueObj in tuples.Select(t => t.GetValueOrDefault(0)).Where(o => o != null)) {
+      metaDatas ??= [
+        sqlDbType == SqlDbType.BigInt
+          ? new SqlMetaData("Value", sqlDbType)
+          : new SqlMetaData("Value", sqlDbType, tuples.Max(t => (t.GetValueOrDefault(0) as string)?.Length ?? 20))
+      ];
+      var castValue = valueObj switch {
+        byte n => (long) n,
+        short n => (long) n,
+        ushort n => (long) n,
+        int n => (long) n,
+        uint n => (long) n,
+        decimal d => (long) d,
+        Enum e => Convert.ToInt64(e),
+        var o => o
+      };
+      SqlDataRecord record = new(metaDatas);
+      record.SetValue(0, castValue);
+      Add(record);
     }
   }
 }
