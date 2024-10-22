ba8b2acedf8bb34fb66914bfcf4ad491a37038bd
Sergei Pavlov <spavlov@servicetitan.com>
2024-10-22 15:19:49 -0700

    Simplify .Zip()

--- Orm/Xtensive.Orm/Linq/Internals/ExpressionComparer.cs
+++ Orm/Xtensive.Orm/Linq/Internals/ExpressionComparer.cs
@@ -109,7 +109,7 @@ namespace Xtensive.Linq
       return VisitNew(x.NewExpression, y.NewExpression)
         && x.Initializers.Count==y.Initializers.Count
         && x.Initializers
-          .Zip(y.Initializers, (first, second) => new Pair<ElementInit>(first, second))
+          .Zip(y.Initializers)
           .All(p => self.VisitElementInit(p.First, p.Second));
     }
 
@@ -125,7 +125,7 @@ namespace Xtensive.Linq
       return VisitNew(x.NewExpression, y.NewExpression)
         && x.Bindings.Count==y.Bindings.Count
         && x.Bindings
-          .Zip(y.Bindings, (first, second) => new Pair<MemberBinding>(first, second))
+          .Zip(y.Bindings)
           .All(p => self.VisitMemberBinding(p.First, p.Second));
     }
 
@@ -145,14 +145,14 @@ namespace Xtensive.Linq
           var mby = (MemberMemberBinding)y;
           return mbx.Bindings.Count==mby.Bindings.Count
                  && mbx.Bindings
-                    .Zip(mby.Bindings, (first, second) => new Pair<MemberBinding>(first, second))
+                    .Zip(mby.Bindings)
                     .All(p => self.VisitMemberBinding(p.First, p.Second));
         case MemberBindingType.ListBinding:
           var mlx = (MemberListBinding)x;
           var mly = (MemberListBinding)y;
           return mlx.Initializers.Count==mly.Initializers.Count
                  && mlx.Initializers
-                    .Zip(mly.Initializers, (first, second) => new Pair<ElementInit>(first, second))
+                    .Zip(mly.Initializers)
                     .All(p => self.VisitElementInit(p.First, p.Second));
         default:
           throw new ArgumentOutOfRangeException();
