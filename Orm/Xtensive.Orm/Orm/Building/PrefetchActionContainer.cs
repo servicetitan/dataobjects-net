b262bb77f531f9796d97cc63cb3c60eb06d577a0
Sergei Pavlov <spavlov@servicetitan.com>
2024-10-25 16:46:06 -0700

    Optimize `PrefetchActionContainer`

--- Orm/Xtensive.Orm/Orm/Building/PrefetchActionContainer.cs
+++ Orm/Xtensive.Orm/Orm/Building/PrefetchActionContainer.cs
@@ -4,41 +4,27 @@
 // Created by: Alexis Kochetov
 // Created:    2010.01.27
 
-using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using Xtensive.Core;
 using Xtensive.Orm.Internals.Prefetch;
 using Xtensive.Orm.Model;
 using Xtensive.Orm.Providers;
 
-namespace Xtensive.Orm.Building
+namespace Xtensive.Orm.Building;
+
+[Serializable]
+internal static class PrefetchActionContainer
 {
-  [Serializable]
-  internal sealed class PrefetchActionContainer
+  // Returns null if associations is empty
+  public static Action<SessionHandler, IEnumerable<Key>> BuildPrefetchAction(TypeInfo type, IEnumerable<AssociationInfo> associations)
   {
-    private readonly TypeInfo type;
-    private IReadOnlyList<PrefetchFieldDescriptor> fields;
+    var fields = associations.Select(static association => new PrefetchFieldDescriptor(association.OwnerField, true, false)).ToArray();
+    return fields.Length > 0
+      ? Prefetch
+      : null;
 
-    // Returns null if associations is empty
-    public Action<SessionHandler, IEnumerable<Key>> BuildPrefetchAction(IEnumerable<AssociationInfo> associations)
-    {
-      fields = associations.Select(static association => new PrefetchFieldDescriptor(association.OwnerField, true, false))
-        .ToList();
-      return fields.Count > 0 ? Prefetch : null;
-    }
-
-    private void Prefetch(SessionHandler sh, IEnumerable<Key> keys)
+    void Prefetch(SessionHandler sh, IEnumerable<Key> keys)
     {
       foreach (var key in keys)
         sh.Prefetch(key, type, fields);
-    }
-
-    public PrefetchActionContainer(TypeInfo type)
-    {
-      this.type = type;
-      fields = Array.Empty<PrefetchFieldDescriptor>();
     }
   }
 }
