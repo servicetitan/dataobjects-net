2003ff5c61fcb74279cc4be4563739bcea79d38d
Sergei Pavlov <spavlov@servicetitan.com>
2025-01-10 08:48:42 -0800

    ConcatUnionBaseProvider

--- Orm/Xtensive.Orm/Orm/Rse/Providers/Compilable/ConcatProvider.cs
+++ Orm/Xtensive.Orm/Orm/Rse/Providers/Compilable/ConcatProvider.cs
@@ -6,67 +6,63 @@
 
 using Xtensive.Collections;
 
-namespace Xtensive.Orm.Rse.Providers
+namespace Xtensive.Orm.Rse.Providers;
+
+public abstract class ConcatUnionBaseProvider : BinaryProvider
 {
-  /// <summary>
-  /// Produces concatenation between <see cref="BinaryProvider.Left"/> and 
-  /// <see cref="BinaryProvider.Right"/> sources.
-  /// </summary>
-  [Serializable]
-  public sealed class ConcatProvider : BinaryProvider
+  protected abstract void EnsureOperationIsPossible();
+
+  protected override RecordSetHeader BuildHeader()
   {
-    protected override RecordSetHeader BuildHeader()
-    {
-      EnsureConcatIsPossible();
-      HashSet<ColNum> mappedColumnIndexes = [];
-      var leftHeader = Left.Header;
-      var leftHeaderColumns = leftHeader.Columns;
-      var rightHeaderColumns = Right.Header.Columns;
-      var columns = new Column[leftHeaderColumns.Count];
-      for (ColNum i = 0; i < columns.Length; i++) {
-        var leftColumn = leftHeaderColumns[i];
-        var rightColumn = rightHeaderColumns[i];
-        if (leftColumn is MappedColumn leftMappedColumn
-            && rightColumn is MappedColumn rightMappedColumn
-            && leftMappedColumn.ColumnInfoRef.Equals(rightMappedColumn.ColumnInfoRef)) {
-          columns[i] = leftMappedColumn;
-          mappedColumnIndexes.Add(i);
-        }
-        else
-          columns[i] = new SystemColumn(leftColumn.Name, leftColumn.Index, leftColumn.Type);
+    EnsureOperationIsPossible();
+    HashSet<ColNum> mappedColumnIndexes = [];
+    var leftHeader = Left.Header;
+    var leftHeaderColumns = leftHeader.Columns;
+    var rightHeaderColumns = Right.Header.Columns;
+    var columns = new Column[leftHeaderColumns.Count];
+    for (ColNum i = 0; i < columns.Length; i++) {
+      var leftColumn = leftHeaderColumns[i];
+      var rightColumn = rightHeaderColumns[i];
+      if (leftColumn is MappedColumn leftMappedColumn
+          && rightColumn is MappedColumn rightMappedColumn
+          && leftMappedColumn.ColumnInfoRef.Equals(rightMappedColumn.ColumnInfoRef)) {
+        columns[i] = leftMappedColumn;
+        mappedColumnIndexes.Add(i);
       }
-      var columnGroups = leftHeader.ColumnGroups.Where(cg => mappedColumnIndexes.IsSupersetOf(cg.Keys)).ToArray();
-
-      return new RecordSetHeader(
-        leftHeader.TupleDescriptor,
-        columns,
-        columnGroups,
-        null,
-        null);
+      else
+        columns[i] = new SystemColumn(leftColumn.Name, leftColumn.Index, leftColumn.Type);
     }
+    var columnGroups = leftHeader.ColumnGroups.Where(cg => mappedColumnIndexes.IsSupersetOf(cg.Keys)).ToArray();
 
-    /// <exception cref="InvalidOperationException">Something went wrong.</exception>
-    private void EnsureConcatIsPossible()
-    {
-      var left = Left.Header.TupleDescriptor;
-      var right = Right.Header.TupleDescriptor;
-      if (!left.Equals(right))
-        throw new InvalidOperationException(String.Format(Strings.ExXCantBeExecuted, "Concatenation"));
-    }
-
-    internal override Provider Visit(ProviderVisitor visitor) => visitor.VisitConcat(this);
-
-    // Constructors
-
-    /// <summary>
-    ///  Initializes a new instance of this class.
-    /// </summary>
-    /// <param name="left">The left provider to intersect.</param>
-    /// <param name="right">The right provider to intersect.</param>
-    public ConcatProvider(CompilableProvider left, CompilableProvider right)
-      : base(ProviderType.Concat, left, right)
-    {
-      Initialize();
-    }
+    return new RecordSetHeader(
+      leftHeader.TupleDescriptor,
+      columns,
+      columnGroups,
+      null,
+      null);
   }
+
+  protected ConcatUnionBaseProvider(ProviderType type, CompilableProvider left, CompilableProvider right)
+    : base(type, left, right)
+  {
+    Initialize();
+  }
+}
+
+/// <summary>
+/// Produces concatenation between <see cref="BinaryProvider.Left"/> and 
+/// <see cref="BinaryProvider.Right"/> sources.
+/// </summary>
+[Serializable]
+public sealed class ConcatProvider(CompilableProvider left, CompilableProvider right)
+  : ConcatUnionBaseProvider(ProviderType.Concat, left, right)
+{
+  /// <exception cref="InvalidOperationException">Something went wrong.</exception>
+  protected override void EnsureOperationIsPossible()
+  {
+    if (!Left.Header.TupleDescriptor.Equals(Right.Header.TupleDescriptor))
+      throw new InvalidOperationException(String.Format(Strings.ExXCantBeExecuted, "Concatenation"));
+  }
+
+  internal override Provider Visit(ProviderVisitor visitor) => visitor.VisitConcat(this);
 }
