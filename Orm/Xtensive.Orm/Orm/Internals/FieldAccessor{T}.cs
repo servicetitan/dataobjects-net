8752275a3f6547f699e762f81b69fcb3855a10b8
Sergei Pavlov <spavlov@servicetitan.com>
2025-01-14 13:50:12 -0800

    Remove DefaultValue prop

--- Orm/Xtensive.Orm/Orm/Internals/FieldAccessor{T}.cs
+++ Orm/Xtensive.Orm/Orm/Internals/FieldAccessor{T}.cs
@@ -4,13 +4,10 @@
 // Created by: Alexey Gamzov
 // Created:    2008.06.02
 
-
 namespace Xtensive.Orm.Internals;
 
 internal abstract class FieldAccessor<T>() : FieldAccessor(default(T))
 {
-  public T DefaultValue { get; }
-
   public abstract void SetValue(Persistent obj, T value);
   public override void SetUntypedValue(Persistent obj, object value) => SetValue(obj, (T) value);
 
