1fd3edfae7128c58b5720790d6cdff0549c3b732
Sergei Pavlov <spavlov@servicetitan.com>
2025-01-15 08:23:32 -0800

    Revert checking for Non-`public` search for `.IsNullOrEmpty()`

--- Orm/Xtensive.Orm/Orm/Linq/MemberCompilation/MemberCompilerProvider.cs
+++ Orm/Xtensive.Orm/Orm/Linq/MemberCompilation/MemberCompilerProvider.cs
@@ -184,7 +184,7 @@ namespace Xtensive.Orm.Linq.MemberCompilation
             Strings.ExCompilerXHasInvalidTargetType, compiler.GetFullName(true)));
 
       var parameterTypes = ValidateCompilerParametersAndExtractTargetSignature(compiler, isGeneric);
-      var bindingFlags = BindingFlags.Public | BindingFlags.NonPublic;
+      var bindingFlags = BindingFlags.Public;
 
       if (isCtor)
         bindingFlags |= BindingFlags.Instance;
