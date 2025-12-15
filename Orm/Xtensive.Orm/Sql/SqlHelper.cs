be0b904fb15bd1c461eab33e85cac39f62f4e13e
Sergei Pavlov <spavlov@servicetitan.com>
2025-12-15 11:56:12 -0800

    Optimize NotifyConnectionOpening

--- Orm/Xtensive.Orm/Sql/SqlHelper.cs
+++ Orm/Xtensive.Orm/Sql/SqlHelper.cs
@@ -583,9 +583,10 @@ namespace Xtensive.Sql
     public static async Task NotifyConnectionOpeningAsync(
       IEnumerable<IDbConnectionAccessor> connectionAccessors, DbConnection connection, bool reconnect = false, CancellationToken token = default)
     {
+      ConnectionEventData eventData = null;
       foreach (var accessor in connectionAccessors) {
         await accessor.ConnectionOpeningAsync(
-          new ConnectionEventData(connection, reconnect), token)
+            eventData ??= new ConnectionEventData(connection, reconnect), token)
           .ConfigureAwaitFalse();
       }
     }
